af0446c1eda1eb2f9c49302c8f92967d4700dc0f
nermindonat <nermingurses21@gmail.com>
2021-03-16 13:08:51 +0300

    15. Gün yetkilendirme

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,8 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
@@ -41,8 +44,6 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();     //bana arka planda bir referans oluþtur. 
             //services.AddSingleton<IProductDal, EfProductDal>();
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -59,6 +60,9 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+            services.AddDependencyResolvers(new ICoreModule[] { 
+                new CoreModule()
+            });  //istediðimiz kadar modul atabilelim.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
